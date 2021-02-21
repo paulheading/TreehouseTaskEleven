a4f685daf05f03799752a0fb661198aeb6c0d172
Paul Heading <paul.heading@barbican.org.uk>
2021-02-21 18:33:45 +0000

    package & readme updated

--- README.md
+++ README.md
@@ -1,38 +1,11 @@
+# Treehouse Task Eleven
 
-# Full Stack JavaScript Techdegree v2 - REST API Project
+For this project, I used the popular Express web application framework and a SQL database to create a REST API that lets users create, read, update, and delete items from a school database.
 
-## Overview of the Provided Project Files
+## Instructions
 
-We've supplied the following files for you to use: 
+Open the folder locally in terminal and type `npm install`. Then seed the database by typing `npm run seed`. To test the Express server, type `npm start` and visit `http://localhost:5000` in your web browser.
 
-* The `seed` folder contains a starting set of data for your database in the form of a JSON file (`data.json`) and a collection of files (`context.js`, `database.js`, and `index.js`) that can be used to create your app's database and populate it with data (we'll explain how to do that below).
-* We've included a `.gitignore` file to ensure that the `node_modules` folder doesn't get pushed to your GitHub repo.
-* The `app.js` file configures Express to serve a simple REST API. We've also configured the `morgan` npm package to log HTTP requests/responses to the console. You'll update this file with the routes for the API. You'll update this file with the routes for the API.
-* The `nodemon.js` file configures the nodemon Node.js module, which we are using to run your REST API.
-* The `package.json` file (and the associated `package-lock.json` file) contain the project's npm configuration, which includes the project's dependencies.
-* The `RESTAPI.postman_collection.json` file is a collection of Postman requests that you can use to test and explore your REST API.
+## Screengrab
 
-## Getting Started
-
-To get up and running with this project, run the following commands from the root of the folder that contains this README file.
-
-First, install the project's dependencies using `npm`.
-
-```
-npm install
-
-```
-
-Second, seed the SQLite database.
-
-```
-npm run seed
-```
-
-And lastly, start the application.
-
-```
-npm start
-```
-
-To test the Express server, browse to the URL [http://localhost:5000/](http://localhost:5000/).
+![screengrab](https://repository-images.githubusercontent.com/256119071/bc596100-7472-11eb-8cb0-c692bae7ffa5)
--- routes/api.js
+++ routes/api.js
@@ -1,42 +1,34 @@
+const express = require("express"),
+  bcryptjs = require("bcryptjs"),
+  auth = require("basic-auth"),
+  legit = require("legit"),
+  router = express.Router(),
+  { sequelize, User, Course } = require("../sequelize"),
+  { check, validationResult } = require("express-validator");
 
-const express  = require('express'),
-      bcryptjs = require('bcryptjs'),
-      auth     = require('basic-auth'),
-      legit    = require('legit'),
-      router   = express.Router(),      
-      { sequelize, User, Course } = require('../sequelize'),
-      { check, validationResult } = require('express-validator');
-
-const titleCheck = check('title')
-.exists({ checkNull : true, checkFalsy : true })
-.withMessage('Please provide a value for "title"'),
-
-descriptionCheck = check('description')
-.exists({ checkNull : true, checkFalsy : true })
-.withMessage('Please provide a value for "description"'),
-
-userIdCheck = check('userId')
-.exists({ checkNull : true, checkFalsy : true })
-.withMessage('Please provide a value for "userId"'),
-
-firstNameCheck = check('firstName')
-.exists({ checkNull : true, checkFalsy : true })
-.withMessage('Please provide a value for "firstName"'),
-
-lastNameCheck = check('lastName')
-.exists({ checkNull : true, checkFalsy : true })
-.withMessage('Please provide a value for "lastName"'),
-
-emailAddressCheck = check('emailAddress')
-.exists({ checkNull : true, checkFalsy : true })
-.withMessage('Please provide a value for "emailAddress"'),
-
-passwordCheck = check('password')
-.exists({ checkNull : true, checkFalsy : true })
-.withMessage('Please provide a value for "password"');
+const titleCheck = check("title")
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "title"'),
+  descriptionCheck = check("description")
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "description"'),
+  userIdCheck = check("userId")
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "userId"'),
+  firstNameCheck = check("firstName")
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "firstName"'),
+  lastNameCheck = check("lastName")
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "lastName"'),
+  emailAddressCheck = check("emailAddress")
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "emailAddress"'),
+  passwordCheck = check("password")
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "password"');
 
 const authenticateUser = asyncHandler(async (req, res, next) => {
-
   let message = null;
 
   // get user credentials from header
@@ -45,432 +37,354 @@ const authenticateUser = asyncHandler(async (req, res, next) => {
   if (credentials) {
     // look for user in database by emailAddress
     // check for match with crudentials
-    let user = await User.findAll({ where : { emailAddress : credentials.name }});
-    
+    let user = await User.findAll({
+      where: { emailAddress: credentials.name },
+    });
+
     user = user[0];
 
     if (user) {
       // compare database + header passwords with bcrypt
-      const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
-          
-      if (authenticated) {
+      const authenticated = bcryptjs.compareSync(
+        credentials.pass,
+        user.password
+      );
 
+      if (authenticated) {
         // keep messaging vague
-        console.log(`Authentication successful for emailAddress: ${user.emailAddress}`);
+        console.log(
+          `Authentication successful for emailAddress: ${user.emailAddress}`
+        );
 
         // store the database user info on req so any other middleware can access the info
         req.currentUser = user;
-        
       } else {
-
         // keep messaging vague
-        message = `Authentication failure for emailAddress: ${user.emailAddress}`;                
-
+        message = `Authentication failure for emailAddress: ${user.emailAddress}`;
       }
-
     } else {
-
       // keep messaging vague
       message = `User not found for emailAddress: ${credentials.emailAddress}`;
-
     }
-
   } else {
-
     // keep messaging vague
-    message = 'Auth header not found';
-
+    message = "Auth header not found";
   }
 
   if (message) {
-
     console.warn(message);
 
     // keep messaging vague
-    res.status(400).json({ message : 'Access Denied' });
-    
+    res.status(400).json({ message: "Access Denied" });
   } else {
-
     next();
-
   }
-
 });
 
 const isEmailValid = asyncHandler(async (req, res, next) => {
-
   try {
-
     // search for result against the legit regex
     const response = await legit(req.body.emailAddress);
 
     if (response.isValid) {
-
       // if the response is valid, go ahead
-      next();      
-      
+      next();
     } else {
-
-      // if the response isn't valid, send a bad status/message  
-      res.status(400).json({ message : "Email address doesn't check out" });
-
+      // if the response isn't valid, send a bad status/message
+      res.status(400).json({ message: "Email address doesn't check out" });
     }
-
   } catch (error) {
-
     res.status(400).json(error);
-
   }
-
 });
 
 /* Handler function to wrap each route. */
 
-function asyncHandler(cb){
-  return async(req, res, next) => {
+function asyncHandler(cb) {
+  return async (req, res, next) => {
     try {
       await cb(req, res, next);
-    } catch(error){
+    } catch (error) {
       res.status(500).send(error);
     }
-  }
+  };
 }
 
 /* GET api listing. */
 
-router.get('/', asyncHandler(async (req, res) => {
-
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-    res.sendStatus(200);   
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-    res.sendStatus(500);
-  }
-
-}));
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+      res.sendStatus(200);
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+      res.sendStatus(500);
+    }
+  })
+);
 
 // ********** ********** ********** USER ROUTES ********** ********** **********
 
-router.get('/users', authenticateUser, 
-                     asyncHandler(async (req, res) => {
-  
-  // currentUser is set by authenticateUser
-  const user = req.currentUser;
+router.get(
+  "/users",
+  authenticateUser,
+  asyncHandler(async (req, res) => {
+    // currentUser is set by authenticateUser
+    const user = req.currentUser;
 
-  // returns the currently authenticated user
-  res.status(200).json({
-    id : user.id,
-    firstName : user.firstName,
-    lastName : user.lastName,
-    emailAddress : user.emailAddress
-  });
+    // returns the currently authenticated user
+    res.status(200).json({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      emailAddress: user.emailAddress,
+    });
+  })
+);
 
-}));
+router.post(
+  "/users",
+  firstNameCheck,
+  lastNameCheck,
+  emailAddressCheck,
+  passwordCheck,
+  isEmailValid,
+  asyncHandler(async (req, res) => {
+    // check validation for errors
+    const errors = validationResult(req);
 
-router.post('/users', firstNameCheck,
-                      lastNameCheck,
-                      emailAddressCheck,
-                      passwordCheck,
-                      isEmailValid,
-                      asyncHandler(async (req, res) => {
+    // if errors are found
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
 
-  // check validation for errors
-  const errors = validationResult(req);
+      // return bad request status + display errors
+      res.status(400).json({ errors: errorMessages });
+    } else {
+      try {
+        const user = req.body;
 
-  // if errors are found
-  if (!errors.isEmpty()) {
+        // hash the new user's password
+        user.password = bcryptjs.hashSync(user.password);
 
-    const errorMessages = errors.array().map(error => error.msg);
+        await User.create(user);
 
-    // return bad request status + display errors
-    res.status(400).json({ errors : errorMessages });
-    
-  } else {
-
-    try {
-
-      const user = req.body;
-
-      // hash the new user's password
-      user.password = bcryptjs.hashSync(user.password);
-
-      await User.create(user);
-
-      // return no content
-      res.header('Location','/').sendStatus(201);
-  
-    } catch (error) {
-  
-      if (error.name === "SequelizeValidationError") {
-  
-        res.status(400).json({ errors : error.errors });
-  
-      } else {
-  
-        throw error;
-  
+        // return no content
+        res.header("Location", "/").sendStatus(201);
+      } catch (error) {
+        if (error.name === "SequelizeValidationError") {
+          res.status(400).json({ errors: error.errors });
+        } else {
+          throw error;
+        }
       }
-  
     }
-
-  }
-
-}));
+  })
+);
 
 // ********** ********** ********** COURSE ROUTES ********** ********** **********
 
-router.get('/courses', asyncHandler(async (req, res) => {
+router.get(
+  "/courses",
+  asyncHandler(async (req, res) => {
+    // find all courses in database
+    const courses = await Course.findAll({
+      attributes: [
+        "id",
+        "title",
+        "description",
+        "estimatedTime",
+        "materialsNeeded",
+        "userId",
+      ],
+    });
 
-  // find all courses in database
-  const courses = await Course.findAll({
-    attributes : [
-      "id",
-      "title",
-      "description",
-      "estimatedTime",
-      "materialsNeeded",
-      "userId"
-    ]
-  });
-
-  try {
-
-    // if courses exist
-    if (courses) {
-
-      // return a successful status
-      res.status(200).json(courses);
-      
-    } else {
-
-      // return not found
-      res.sendStatus(404);
-
-    }    
-
-  } catch (error) {
-
-    throw error;
-
-  }
-
-}));
-
-router.get('/courses/:id', asyncHandler(async (req, res) => {
-
-  // find course in database
-  const course = await Course.findAll({
-    where : {
-      "id" : req.params.id
-    },
-    attributes : 
-    [ "id",
-      "title",
-      "description",
-      "estimatedTime",
-      "materialsNeeded",
-      "userId" ]
-  });
-
-  try {   
-
-    // if course exists
-    if(course) {
-
-      // return a successful status
-      res.status(200).json(course);
-
-    } else {
-
-      // return not found
-      res.sendStatus(404);
-
-    }
-
-  } catch (error) {
-
-    throw error;
-
-  }
-
-}));
-
-router.post('/courses', authenticateUser,
-                        titleCheck, 
-                        descriptionCheck,
-                        userIdCheck,
-                        asyncHandler(async (req, res) => {
-
-  // check validation for errors
-  const errors = validationResult(req);
-
-  // if there are any errors
-  if (!errors.isEmpty()) {
-
-    const errorMessages = errors.array().map(error => error.msg);
-    // map and print them as json
-    res.status(400).json({ errors : errorMessages });  
-
-  } else {
-    
     try {
+      // if courses exist
+      if (courses) {
+        // return a successful status
+        res.status(200).json(courses);
+      } else {
+        // return not found
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      throw error;
+    }
+  })
+);
 
-      // create database record
-      await Course.create(req.body);
+router.get(
+  "/courses/:id",
+  asyncHandler(async (req, res) => {
+    // find course in database
+    const course = await Course.findAll({
+      where: {
+        id: req.params.id,
+      },
+      attributes: [
+        "id",
+        "title",
+        "description",
+        "estimatedTime",
+        "materialsNeeded",
+        "userId",
+      ],
+    });
+
+    try {
+      // if course exists
+      if (course) {
+        // return a successful status
+        res.status(200).json(course);
+      } else {
+        // return not found
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      throw error;
+    }
+  })
+);
+
+router.post(
+  "/courses",
+  authenticateUser,
+  titleCheck,
+  descriptionCheck,
+  userIdCheck,
+  asyncHandler(async (req, res) => {
+    // check validation for errors
+    const errors = validationResult(req);
+
+    // if there are any errors
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      // map and print them as json
+      res.status(400).json({ errors: errorMessages });
+    } else {
+      try {
+        // create database record
+        await Course.create(req.body);
+
+        try {
+          let match = {};
+
+          match.find = await Course.findAll({
+            // only 1 result please
+            limit: 1,
+            // base search on mulitple criteria
+            where: {
+              title: req.body.title,
+              description: req.body.description,
+              userId: req.body.userId,
+            },
+            // just the id please
+            attributes: ["id"],
+          });
+
+          // create that redirect
+          match.print = `/courses/${match.find[0].id}`;
+
+          // returns redirect + no content
+          res.header("Location", match.print).sendStatus(201);
+        } catch (error) {
+          throw error;
+        }
+      } catch (error) {
+        if (error.name === "SequelizeValidationError") {
+          res.status(400).json({ errors: error.errors });
+        } else {
+          throw error;
+        }
+      }
+    }
+  })
+);
+
+router.put(
+  "/courses/:id",
+  authenticateUser,
+  titleCheck,
+  descriptionCheck,
+  userIdCheck,
+  asyncHandler(async (req, res) => {
+    // check validation for errors
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+
+      // return bad request status + display errors
+      res.status(400).json({ errors: errorMessages });
+    } else {
+      // find course in database
+      const course = await Course.findByPk(req.params.id);
 
       try {
-
-        let match = {};
-
-        match.find = await Course.findAll({
-          // only 1 result please
-          limit : 1,
-          // base search on mulitple criteria
-          where : {
-            "title" : req.body.title,
-            "description" : req.body.description,
-            "userId" : req.body.userId
-          },
-          // just the id please
-          attributes : 
-          [ "id"]
-        });
-
-        // create that redirect  
-        match.print = `/courses/${match.find[0].id}`;
-  
-        // returns redirect + no content
-        res.header('Location',match.print).sendStatus(201);
-        
+        // if course exists
+        if (course) {
+          // check authentication data against course data
+          // if the id's match, then we're good
+          if (req.currentUser.id == course.userId) {
+            // set which columns are available for update
+            course
+              .update({
+                userId: req.body.userId,
+                title: req.body.title,
+                description: req.body.description,
+              })
+              .then(() => {
+                // return status no content
+                res.sendStatus(204);
+              });
+          } else {
+            // if the id's don't match, this isn't the owner!
+            res.status(403).json({ message: "Access Denied" });
+          }
+        } else {
+          // return not found
+          res.sendStatus(404);
+        }
       } catch (error) {
-
-        throw error;
-        
+        if (error.name === "SequelizeValidationError") {
+          res.status(500).json({ errors: error.errors });
+        } else {
+          throw error;
+        }
       }
-      
-    } catch (error) {
-
-      if (error.name === "SequelizeValidationError") {
-
-        res.status(400).json({ errors : error.errors });
-  
-      } else {
-  
-        throw error;
-  
-      }
-      
     }
+  })
+);
 
-  }
-
-}));
-
-router.put('/courses/:id', authenticateUser,
-                           titleCheck, 
-                           descriptionCheck,
-                           userIdCheck,
-                           asyncHandler(async (req, res) => {
-
-  // check validation for errors
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-
-    const errorMessages = errors.array().map(error => error.msg);
-
-    // return bad request status + display errors
-    res.status(400).json({ errors : errorMessages });
-    
-  } else {
-
+router.delete(
+  "/courses/:id",
+  authenticateUser,
+  asyncHandler(async (req, res) => {
     // find course in database
     const course = await Course.findByPk(req.params.id);
 
-    try {
+    // if course exists
+    if (course) {
+      // check authentication data against course data
+      // if the id's match, then we're good
+      if (req.currentUser.id == course.userId) {
+        // delete course
+        await course.destroy();
 
-      // if course exists
-      if (course) {
-
-        // check authentication data against course data
-        // if the id's match, then we're good
-        if (req.currentUser.id == course.userId) {
-
-          // set which columns are available for update
-          course.update({ 
-            userId : req.body.userId,
-            title : req.body.title,
-            description : req.body.description
-          }).then(() => {
-            // return status no content
-            res.sendStatus(204);
-          });
-
-        } else {
-          
-          // if the id's don't match, this isn't the owner!
-          res.status(403).json({ message : 'Access Denied' });
-        
-        }
-
+        // return no content
+        res.sendStatus(204);
       } else {
-
-        // return not found
-        res.sendStatus(404);
-
+        // if the id's don't match, this isn't the owner!
+        res.status(403).json({ message: "Access Denied" });
       }
-    
-    } catch (error) {
-      
-      if (error.name === "SequelizeValidationError") {
-
-        res.status(500).json({ errors : error.errors });
-
-      } else {
-
-        throw error;
-
-      }
-    }
-  }
-
-}));
-
-router.delete('/courses/:id', authenticateUser,
-                              asyncHandler(async (req, res) => {
-
-  // find course in database
-  const course = await Course.findByPk(req.params.id);
-
-  // if course exists
-  if (course) {
-
-    // check authentication data against course data
-    // if the id's match, then we're good
-    if (req.currentUser.id == course.userId) {
-
-      // delete course
-      await course.destroy();
-
-      // return no content
-      res.sendStatus(204);
-
     } else {
-
-      // if the id's don't match, this isn't the owner!
-      res.status(403).json({ message : 'Access Denied' });
-
+      // return not found
+      res.sendStatus(404);
     }
-
-  } else {
-
-    // return not found
-    res.sendStatus(404);
-
-  }  
-
-}));
+  })
+);
 
 module.exports = router;
